83df55dbf5b6c6a3d6da01fa4119ff82b7769951
blabla1234-cc <longkhongsad26@gmail.com>
2026-03-10 21:36:05 +0700

    Update Student.h

--- include/Student.h
+++ include/Student.h
@@ -6,11 +6,11 @@
 
 class Student {
 private:
-    std::string m_studentID;     
+    std::string m_studentID;
     std::string m_studentName;
-    int m_studentBirthYear;            
+    int m_studentBirthYear;      // Đã đổi tên
     std::string m_studentMajor;
-    double m_studentGPA;        
+    double m_studentGPA;         // Kiểu double
     std::string m_studentNumber;
 
 public:
@@ -21,17 +21,15 @@ public:
     // Getters
     std::string getStudentID() const;
     std::string getStudentName() const;
-    int getStudentBirthYear() const;    // Hàm lấy năm sinh
-    std::string getStudentMajor() const;
+    int getStudentBirthYear() const;
     double getStudentGPA() const;
     std::string getStudentNumber() const;
 
-    // Setters - Đã sửa thành const std::string& để tối ưu bộ nhớ
+    // Setters 
     void setStudentID(const std::string& id);
     void setStudentName(const std::string& name);
     void setStudentBirthYear(int birthYear);
     void setStudentMajor(const std::string& major);
-    void setStudentGPA(double gpa);
     void setStudentNumber(const std::string& phone);
 
     void displayStudentInfo() const;
