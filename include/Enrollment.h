28319e3798e068c8f8889e487d03666ee0a0ab90
phongforest <phongforest@gmail.com>
2026-03-11 20:57:23 +0700

    Update Enrollment.h

--- include/Enrollment.h
+++ include/Enrollment.h
@@ -8,15 +8,22 @@
 #include "Student.h"
 #include "Course.h"
 
-struct StudentEnrollment {
-    std::shared_ptr<Student> m_ptrStudent;
-    std::shared_ptr<Course> m_ptrCourse;
-    double m_courseGrade;
-    StudentEnrollment(): m_courseGrade(0.0) {}
-    StudentEnrollment(std::shared_ptr<Student> ptrStudent, std::shared_ptr<Course> ptrCourse):
-    m_ptrStudent(ptrStudent), m_ptrCourse(ptrCourse), m_courseGrade(0.0) {}
-};
-
+class StudentEnrollment 
+{
+    private:
+        std::shared_ptr<Student> m_ptrStudent;
+        std::shared_ptr<Course> m_ptrCourse;
+        double m_courseGrade;
+    public:
+        StudentEnrollment(): m_courseGrade(0.0) {}
+        StudentEnrollment(std::shared_ptr<Student> ptrStudent, std::shared_ptr<Course> ptrCourse):
+        m_ptrStudent(ptrStudent), m_ptrCourse(ptrCourse), m_courseGrade(0.0) {}
+    //getters and setters
+        std::shared_ptr<Student> getStudent() const { return m_ptrStudent; }
+        std::shared_ptr<Course> getCourse() const { return m_ptrCourse; }
+        double getCourseGrade() const { return m_courseGrade; }
+        void setCourseGrade(double grade) { m_courseGrade = grade; }
+}
 void CourseEnroll(std::shared_ptr<Student> ptrStudent, std::shared_ptr<Course> ptrCourse);
 double getCourseGrade(std::shared_ptr<Student> ptrStudent, std::shared_ptr<Course> ptrCourse);
 double getStudentGPA(std::shared_ptr<Student> ptrStudent);
